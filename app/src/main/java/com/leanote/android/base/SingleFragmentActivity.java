e539f111bf89e626159e8d6df41e1d5edea431f9
xingxing <xxx823952375@gmail.com>
2017-03-05 18:35:26 +0800

    添加基类

---
+++ app/src/main/java/com/leanote/android/base/SingleFragmentActivity.java
@@ -0,0 +1,30 @@
+package com.leanote.android.base;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.leanote.android.R;
+
+/**
+ * Created by xiongxingxing on 17/3/5.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+}
