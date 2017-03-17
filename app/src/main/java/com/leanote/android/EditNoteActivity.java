930b4f45aefa3e72adcbd25d90ccbb856ba0bfb1
xingxing <xxx823952375@gmail.com>
2017-03-17 22:31:58 +0800

    编辑笔记

--- app/src/main/java/com/leanote/android/EditNoteActivity.java
+++ app/src/main/java/com/leanote/android/EditNoteActivity.java
@@ -26,21 +26,21 @@ import butterknife.OnClick;
  * Created by xiongxingxing on 17/3/5.
  */
 
-public class NoteActivity extends SingleFragmentActivity {
+public class EditNoteActivity extends SingleFragmentActivity {
 
     public static final String ARG_NOTE = "note";
     @Override
     protected Fragment createFragment() {
-        return NoteFragment.newInstance((Note) getIntent().getSerializableExtra(ARG_NOTE));
+        return EditNoteFragment.newInstance((Note) getIntent().getSerializableExtra(ARG_NOTE));
     }
 
-    public static void showPreNote(Context context, Note note) {
-        Intent intent = new Intent(context, NoteActivity.class);
+    public static void showEditNote(Context context, Note note) {
+        Intent intent = new Intent(context, EditNoteActivity.class);
         intent.putExtra(ARG_NOTE, note);
         context.startActivity(intent);
     }
 
-    public static class NoteFragment extends Fragment implements Editor.EditorListener {
+    public static class EditNoteFragment extends Fragment implements Editor.EditorListener {
 
         @BindView(R.id.web_view)
         WebView mWebView;
@@ -61,24 +61,18 @@ public class NoteActivity extends SingleFragmentActivity {
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_note, container, false);
+            View view = inflater.inflate(R.layout.fragment_edit_note, container, false);
+            ViewGroup bottomPanel = (ViewGroup) view.findViewById(R.id.bottom_panel);
+            View formatBar = inflater.inflate(R.layout.format_bar_richtext, bottomPanel, false);
+            bottomPanel.addView(formatBar, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
             ButterKnife.bind(this, view);
             mEditor.init(mWebView);
             return view;
         }
 
-        @OnClick({R.id.menu_edit})
-        void OnClick(View view) {
-            switch (view.getId()) {
-                case R.id.menu_edit:
-                    EditNoteActivity.showEditNote(getContext(), mNote);
-                    break;
-            }
-        }
-
-        public static NoteFragment newInstance(Note note) {
+        public static EditNoteFragment newInstance(Note note) {
             Bundle args = new Bundle();
-            NoteFragment fragment = new NoteFragment();
+            EditNoteFragment fragment = new EditNoteFragment();
             args.putSerializable(ARG_NOTE, note);
             fragment.setArguments(args);
             return fragment;
@@ -88,7 +82,7 @@ public class NoteActivity extends SingleFragmentActivity {
         public void onPageLoaded() {
             mEditor.setTitle(mNote.title);
             mEditor.setContent(mNote.content);
-            mEditor.setEditingEnabled(false);
+            mEditor.setEditingEnabled(true);
         }
 
         @Override
